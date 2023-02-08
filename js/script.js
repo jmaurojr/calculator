3c1a1d554fa1324881e90221d4acd5daaf1bef0e
jmaurojr <112908979+jmaurojr@users.noreply.github.com>
2023-02-08 15:29:38 -0300

    add the toggle between themes

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function insert(num) {
+  var number = document.getElementById("display-terms").innerHTML;
+  document.getElementById("display-terms").innerHTML = number + num;
+}
+
+function clean() {
+  document.getElementById("display-terms").innerHTML = "";
+  document.getElementById("display-results").innerHTML = "";
+}
+
+function backspace() {
+  let results = document.getElementById("display-terms").innerHTML;
+  document.getElementById("display-terms").innerHTML = results.substring(
+    0,
+    results.length - 1
+  );
+}
+
+function calculate() {
+  let results = document.getElementById("display-terms").innerHTML;
+  if (results) {
+    document.getElementById("display-results").innerHTML = eval(results);
+  }
+}
+
+const blueTheme = document.getElementById("blue-theme");
+
+blueTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
